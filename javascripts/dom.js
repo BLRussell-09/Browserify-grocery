9c99c1a15d5a7c60839071f1a4c6a7a13f7f33dc
Barry Russell <blrussell09@gmail.com>
2018-04-14 12:17:13 -0500

    Added event listener

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,13 +1,15 @@
+const events = require('./events');
 
 const printToDom = (domString, divId) => {
   document.getElementById(divId).innerHTML += domString;
+  events.addDepartmentEvents();
 };
 
 const domBuilder = (depArr) => {
   let domString = '';
   depArr.forEach(element => {
     domString += `<div class="col-md-3 department">`;
-    domString += `<h3 class="hide">${element.name}</h3>`;
+    domString += `<h3 class="hide department-title" data-department-id="${element.id}">${element.name}</h3>`;
     domString += `<img class="department-image"src="${element.img}">`;
     domString += `</div>`;
   });
---
+++ javascripts/events.js
@@ -0,0 +1,32 @@
+const departmentCards = document.getElementsByClassName('department');
+
+const showDepartmentName = (e) => {
+  const departmentImg = e.target.children[1];
+  const departmentName = e.target.children[0];
+  departmentName.classList.remove('hide');
+  departmentImg.classList.add('grey-out');
+};
+
+const hideDepartmentName = (e) => {
+  const departmentImg = e.target.children[1];
+  const departmentName = e.target.children[0];
+  departmentName.classList.add('hide');
+  departmentImg.classList.remove('grey-out');
+};
+
+const showItems = (e) => {
+  const departmentId = e.target.parentNode.children[0].dataset.departmentId;
+};
+
+const addDepartmentEvents = () => {
+  for (let idx = 0; idx < departmentCards.length; idx++)
+  {
+    departmentCards[idx].addEventListener('mouseenter', showDepartmentName);
+    departmentCards[idx].addEventListener('mouseleave', hideDepartmentName);
+    departmentCards[idx].addEventListener('click', showItems);
+  }
+};
+
+module.exports = {
+  addDepartmentEvents,
+};
