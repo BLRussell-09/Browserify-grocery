0d96d755fa14716c087ede7bfb016d76c1e72587
Barry Russell <blrussell09@gmail.com>
2018-04-16 19:16:15 -0500

    Updated Items

--- javascripts/data.js
+++ javascripts/data.js
@@ -17,9 +17,20 @@ const setItems = (itemsArray) => {
   items = itemsArray;
 };
 
+const getItemsByDepartment = (departmentId) => {
+  const selectedItems = [];
+  items.forEach(element => {
+    if (element.departmentId === departmentId) {
+      selectedItems.push(element);
+    }
+  });
+  return selectedItems;
+};
+
 module.exports = {
   getdepartments,
   getItems,
   setDepartments,
   setItems,
+  getItemsByDepartment,
 };
